c75d471537a7a411f085a97e4e57af69692aa0ac
rakeshkandhi <rakeshkandhi1432@gmail.com>
2023-11-30 18:25:33 +0530

    configured tailwind settings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import NavBar from './components/NavBar';
 function App() {
     return (
         <>
-            <h1 className="text-4xl font-bold">Hello World</h1>
+            <NavBar />
         </>
     );
 }
